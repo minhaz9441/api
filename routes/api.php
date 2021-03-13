80bfb80cc9a9dfb328f063846fb2fb4bf577be30
minhaz <minhaz.ccna@gmail.com>
2021-03-13 20:58:46 +0530

    Create Model

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource("/items","ItemController");
+
+Route::group(['prefix'=>'items'], function(){
+	Route::apiResource("/{items}/reviews","ReviewController");
+});
